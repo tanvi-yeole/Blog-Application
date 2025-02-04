01cfc30f0ece8b9ea946641311128c2d050cdcf9
tanvi-yeole <111522405+tanvi-yeole@users.noreply.github.com>
2025-02-04 17:35:53 +0530

    implement user login functionality with JWT and create token model

--- Backend/controller/user-controller.js
+++ Backend/controller/user-controller.js
@@ -1,5 +1,10 @@
 import User from "../model/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import Token from "../model/token.js";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 export const signupUser = async (request, response) => {
   try {
@@ -8,22 +13,22 @@ export const signupUser = async (request, response) => {
     const checkUsername = await User.findOne({ username });
     const checkMail = await User.findOne({ email });
 
-    if(checkUsername) {
-        return response.status(409).json({
-            status: 409,
-            message: "Username already exist"
-        })
+    if (checkUsername) {
+      return response.status(409).json({
+        status: 409,
+        message: "Username already exist",
+      });
     }
 
-    if(checkMail) {
-        return response.status(409).json({
-            status: 409,
-            message: "Email already exist"
-        })
+    if (checkMail) {
+      return response.status(409).json({
+        status: 409,
+        message: "Email already exist",
+      });
     }
 
     const hashPass = await bcrypt.hash(password, 12);
-    
+
     const newUser = new User({
       name,
       username,
@@ -36,14 +41,51 @@ export const signupUser = async (request, response) => {
     return response.status(200).json({
       status: 200,
       msg: "Signed up successfully",
-      user: newUser
+      user: newUser,
     });
-    
   } catch (error) {
     return response.status(500).json({
       status: 500,
       error: error,
       msg: "Something went wrong",
     });
+  }
+};
+
+export const loginUser = async (request, response) => {
+  let user = await User.findOne({ username: request.body.username });
+  if (!user) {
+    return response.status(400).json({ msg: "Username does not match" });
+  }
+  try {
+    let match = await bcrypt.compare(request.body.password, user.password);
+    if (match) {
+      const accessToken = jwt.sign(
+        user.toJSON(),
+        process.env.ACCESS_SECRET_KEY,
+        { expiresIn: "15m" }
+      );
+
+      const refreshToken = jwt.sign(
+        user.toJSON(),
+        process.env.REFRESH_SECRET_KEY
+      );
+
+      const newToken = new Token({ token: refreshToken });
+      await newToken.save();
+
+      response.status(200).json({
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+        name: user.name,
+        username: user.username,
+      });
+    } else {
+      return response.status(400).json({ msg: "password does not match" });
+    }
+  } catch (error) {
+    return response.json({
+      error: error.message
+    })
   }
 };
