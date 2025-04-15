0e69c07252b8bac5211548a1fa2d598bc15f5419
tanvi-yeole <111522405+tanvi-yeole@users.noreply.github.com>
2025-04-15 23:40:51 +0530

    fix: remove BrowserRouter wrapper from App component and adjust routing structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Homepage from "./Pages/Homepage";
 import Register from "./Pages/Register";
 import Signin from "./Pages/Signin";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Createblog from "./Pages/Createblog";
 import { useState } from "react";
 import BlogPost from "./Pages/Blog";
@@ -12,15 +12,14 @@ export default function App() {
   return (
     <div>
       <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/login" element={<Signin />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/createblog" element={<Createblog />} />
-          <Route path="/blog/:id" element={<BlogPost />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Signin />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/createblog" element={<Createblog />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
+      </Routes>
     </div>
   );
 }
